dc06479f7b18144f3c86fbc2f5ee86cf8876adba
Yarab <judymotors@gmail.com>
2019-02-16 21:23:48 +0200

    Add 5- Module and Require with NodeJS in Arabic folder

---
+++ 5- Module and Require with NodeJS in Arabic/second.js
@@ -0,0 +1,5 @@
+//I can Access any fil by require method 
+// but I must decler that in variable 
+const first = require('./first');
+console.log(first.getCourses());
+console.log(first.getSessions());
