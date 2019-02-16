e86875ea30a91bd212b36c07c8aecad61b8d9f48
Yarab <judymotors@gmail.com>
2019-02-16 21:19:49 +0200

    Remove app & first & second

--- second.js
+++
@@ -1,5 +0,0 @@
-//I can Access any fil by require method 
-// but I must decler that in variable 
-const first = require('./first');
-console.log(first.getCourses());
-console.log(first.getSessions());
